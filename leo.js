c7c82ce8c55b6a34315662d377bad4b7ea95a800
LeonyMalasanManalili <erlyon15@gmail.com>
2022-05-23 15:27:26 +0200

    First commit - completed

---
+++ leo.js
@@ -0,0 +1,12 @@
+let nome = prompt("Inserisci nome","Name")
+
+let cognome = prompt("Inserisci cognome" , "Surname")
+
+document.getElementById("stampa").innerHTML=`Nome:${nome} <br> Cognome:${cognome}`
+
+// const personalPrompt =(n,c)=>
+// {  
+//    document.getElementById("stampa").innerHTML=`Nome:${n} <br> Cognome:${c}`
+// }
+
+//  personalPrompt(nome,cognome)
